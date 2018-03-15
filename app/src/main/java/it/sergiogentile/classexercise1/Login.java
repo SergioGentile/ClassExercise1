5f883346d6edb26d87cd00ff0ca3e17a9a5e8f80
SergioGentile <sergio.gentile94@gmail.com>
2018-03-15 22:43:53 +0100

    Introduce login constraint without connection on firebase

--- app/src/main/java/it/sergiogentile/classexercise1/Login.java
+++ app/src/main/java/it/sergiogentile/classexercise1/Login.java
@@ -5,21 +5,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 
 public class Login extends AppCompatActivity {
 
+    MaterialEditText edtMail, edtPassword;
     Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        edtMail = (MaterialEditText)findViewById(R.id.edtMail);
+        edtPassword = (MaterialEditText)findViewById(R.id.edtPass);
         btn = (Button)findViewById(R.id.btnLog);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent signIn = new Intent(Login.this, MainActivity.class);
-                startActivity(signIn);
+                //Check with firebase
+                if(edtMail.getText().toString().equals("sergio.gentile94@gmail.com") && edtPassword.getText().toString().equals("25Aprile1994")){
+                    Toast.makeText(Login.this, "Login successfully", Toast.LENGTH_SHORT).show();
+                    Intent signIn = new Intent(Login.this, MainActivity.class);
+                    startActivity(signIn);
+                }
+                else{
+                    Toast.makeText(Login.this, "Login failed", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
